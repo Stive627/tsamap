d10b2aa1724388bbc30beb049afa154bdf560dbd
Stive627 <fossistive627@gmail.com>
2025-05-30 21:13:05 +0530

    now, i use google

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
-import React, {useRef, useEffect, useState} from "react"
-import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css';
-import './App.css'
+import React, {useEffect, useState} from "react"
+import {GoogleMap, useLoadScript} from '@react-google-maps/api'
 
-const INITIAL_CENTER = [
-    0,
-    0
-]
-const INITIAL_ZOOM = 0
-const accessToken = 'pk.eyJ1Ijoic3RpdmV0c2EiLCJhIjoiY21hM3Q4azhvMDBtdjJpcXhpaDRtYjB2OCJ9.cAVLQ69PTYp33gHCB6yV1A'
-
-function App() {
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
-  const [center, setCenter] = useState(INITIAL_CENTER)
-  const [zoom, setZoom] = useState(INITIAL_ZOOM)
-  useEffect(() => {
-    mapboxgl.accessToken = accessToken
-    mapRef.current = new mapboxgl.Map({
-      container:mapContainerRef.current,
-      center:center,
-      zoom:zoom,
-    });
-    mapRef.current.on('move', () => {
-      const mapCenter = mapRef.current.getCenter()
-      const mapZoom = mapRef.current.getZoom()
-      setCenter([mapCenter.lng, mapCenter.lat])
-      setZoom(mapZoom)
-    })
-    return () => {
-      mapRef.current.remove()
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+function App(){ 
+  
+  const KEY_API = 'AIzaSyARg0EOtovWBXVQpcWSwI3I8clW1uMxqic'
+  const [coords, setCoords] = useState({lat:0, lng:0})
+  const {isLoaded} = useLoadScript({googleMapsApiKey:'AIzaSyARg0EOtovWBXVQpcWSwI3I8clW1uMxqic'})
+  console.log(isLoaded)
 
   useEffect(()=>{
     function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    mapRef.current.flyTo({
-      center:[longitude, latitude],
-      zoom: 13.12
-    })
+    setCoords({lat:latitude, lng:longitude})
   }
     function error() {
     console.log("Unable to retrieve your location")
@@ -49,13 +20,9 @@ function App() {
   navigator.geolocation.getCurrentPosition(success, error);
   },[])
 
+  if(!isLoaded) return <p style={{textAlign:'center'}}> Loading..., {isLoaded} {coords.lat} {coords.lng}</p>
   return (
-    <>
-      <div className=" sidebar">
-        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
-      </div>
-      <div id='map-container' ref={mapContainerRef}></div>
-    </>
+    <GoogleMap center={coords} zoom={12} mapContainerStyle={{width:'100%', height:'100vh'}}/>
   )
 }
 
